de5ef1285341f5b625b51d529c95c371045f0905
GitAPhan <itsandrewphan@gmail.com>
2021-11-12 11:21:13 -0700

    copied conditionals example and completed the first practice

--- js/app.js
+++ js/app.js
@@ -16,7 +16,9 @@ var user = {
     is_subscribed: true
 }
 
-// Practice starts here
+
+
+// VARIABLES practice starts here
 
 // Create a variable called player_name and set it to a string
 // Console log the variable and verify your string value shows up:
@@ -85,3 +87,47 @@ for (var i = 0; i < players.length; i++) {
 }
 console.log(players[0]);
 
+
+
+// Conditionals examples
+
+// Create a variable called is_trusted and set it to either true or false.
+// Create an if statement that will console log "We are trusted" if is_trusted is true and "We are not trusted" if is_trusted is false
+var is_trusted = true;
+if (is_trusted) {
+    console.log('We are trusted');
+} else {
+    console.log('We are not trusted');
+}
+
+// Create a variable called tickets_sold and set it to either a number between 0 and 200.
+// Create an if statement that will console log "Many tickets sold" if tickets_sold is more than 100,
+// "A few tickets sold" if tickets_sold is between 50 and 100 and
+// "Not many tickets sold" if tickets_sold is between 0 and 49
+var tickets_sold = 150;
+if (tickets_sold > 100) {
+    console.log('Many tickets sold');
+} else if (tickets_sold >= 50) {
+    console.log('A few tickets sold');
+} else {
+    console.log('Not many tickets sold');
+}
+
+// Create a variable called has_paid and set it to either true or false.
+// Create a variable called items_in_cart and set it to a number between 0 and 10
+// Create an if statement that will console log "Valuable Customer" if has_paid is true AND items_in_cart is at least 5
+var has_paid = true;
+var items_in_cart = 7;
+if (has_paid && items_in_cart >= 5) {
+    console.log('Valuable Customer');
+}
+
+// CONDITIONALS practice starts here
+
+// Using your players variable, create an if statement that will console log the first players name if the first player's is_playing is true
+// Note that the console will log or not depending on what the is_playing of the first player in the array is set to
+for (i = 0; i < players.length; i++) {
+    if(players[i].isPlaying) {
+        console.log(`${players[i].playersName} is playing`);
+    }
+}
